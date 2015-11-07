ec9f7394b6a2f92a3a49ffc313ce673c9095e5d3
Rygaido <rwg6766@NM2550-17.main.ad.rit.edu>
2015-11-07 17:08:09 -0500

    modified AI

--- Assets/ai_movement_test.cs
+++ Assets/ai_movement_test.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class ai_movement_test : MonoBehaviour {
-	
+
 	float direction;
 	float speed;
+	public float speedMax = 4.0f;
+
+	float rot;
+	public float rotSpeed=1.0f;
 	
 	float t;
 	float timer;
+	public float timeMax = 2.0f;
+	float delay;
+	public float delayMax=3.0f;
 	
 	Random rand;
 	
@@ -18,21 +25,33 @@ public class ai_movement_test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (t <= 0) {
-			direction = Random.Range (0.01f, 360f);
-			speed = Random.Range (-5, 5);
+		if (t <= -delay) {
+			direction = Random.Range (-180f, 180f);
+			speed = Random.Range (-speedMax, speedMax);
+			delay = Random.Range(0,delayMax);
 			if (speed < 0) {
 				speed = 0;
 			}
-			timer = Random.Range (0, 5);
+			timer = Random.Range (1, timeMax);
 			t = timer;
-			
-			transform.Rotate(direction,0,0);
+			//rotSpeed = 0.5f;
+			//transform.Rotate(direction,0,0);
+			//transform.Rotate(transform.forward,direction);
+			transform.Rotate(transform.up,direction);
+		}
+
+		if (t > 0) {
+
+			if(rot < Mathf.Abs(direction)){
+				float d=(direction/rotSpeed) * Time.deltaTime;
+				//float d=(rotSpeed/direction) * Time.deltaTime;
+				rot += Mathf.Abs(d);
+				transform.Rotate(transform.up, d);
+			}
+			transform.Translate (transform.forward * (speed * Time.deltaTime));
+			//direction = Vector3.Rotate(Vector3 (1, 0, 0),);
 		}
 		
-		transform.Translate (Vector3.forward * (speed*Time.deltaTime));
 		t -= Time.deltaTime;
-		//direction = Vector3.Rotate(Vector3 (1, 0, 0),);
 	}
 }
