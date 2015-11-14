af0c34679e30098da9dcaaff284702e14530ca4e
Gwendolyn Hart <glh1163@rit.edu>
2015-11-14 17:26:38 -0500

    Door opening interaction finished

--- Assets/PlayerInteractScript.cs
+++ Assets/PlayerInteractScript.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class PlayerInteractScript : MonoBehaviour {
 
+	public float interactDist = 1;
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown("e")) {
 			RaycastHit hitInfo;
-			if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0)), out hitInfo, 100)) {
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0)), out hitInfo, interactDist)) {
 				if (hitInfo.collider.tag == "Door") {
-					Debug.Log ("hardy-har");
-					hitInfo.collider.GetComponent<Animator>().Play("Cube.001");
+					hitInfo.collider.transform.parent.GetComponent<Animation>().Play ();
 				}
 			}
 		}
