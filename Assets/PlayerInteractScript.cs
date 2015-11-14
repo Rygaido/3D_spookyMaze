09a5f02beeb1fe25506ca74315b4cff7832c0d7d
Gwendolyn Hart <glh1163@rit.edu>
2015-11-14 16:52:48 -0500

    Partial implementation for door-opening

---
+++ Assets/PlayerInteractScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("e")) {
+			RaycastHit hitInfo;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0)), out hitInfo, 100)) {
+				if (hitInfo.collider.tag == "Door") {
+					Debug.Log ("hardy-har");
+					hitInfo.collider.GetComponent<Animator>().Play("Cube.001");
+				}
+			}
+		}
+	}
+}
