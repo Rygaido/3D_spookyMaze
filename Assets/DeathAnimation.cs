2227cbbda2825d18b13e938cc59774ca83f04ee2
Gwendolyn Hart <glh1163@rit.edu>
2015-12-11 02:06:39 -0500

    level finished

--- Assets/DeathAnimation.cs
+++ Assets/DeathAnimation.cs
@@ -8,42 +8,51 @@ public class DeathAnimation : MonoBehaviour {
 	float frame = 0;
 	public float fps = 30;
 	
-	Texture2D[] images;
+	Texture2D[][] images;
+
+	int movie = 0;
 
 	// Use this for initialization
 	void Start () {
-		images = Resources.LoadAll<Texture2D>("cutscenes/Death01");
+		images = new Texture2D[2][];
+		images[0] = Resources.LoadAll<Texture2D>("cutscenes/Death01");
+		images[1] = Resources.LoadAll<Texture2D>("cutscenes/Death02");
 	}
 	
 	void Update() {
 		if (playing) {
 			frame += fps * Time.deltaTime;
 			
-			if (frame >= images.Length) {
+			if (frame >= images[movie].Length) {
 				Application.LoadLevel("Scene1");
 			}
 		}
-		
-		// for testing
-		/*if (Input.GetKeyDown("f")) {
-			Play();
-		}*/
 	}
 	
 	// Update is called once per frame
 	void OnGUI () {
 		if (playing) {
-			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), images[(int)frame]);
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), images[movie][(int)frame]);
 		}
 	}
 
 	void OnCollisionEnter(Collision col) {
 		if (col.collider.tag == "Enemy") {
-			Play();
+			movieValue val = col.collider.GetComponent<movieValue>();
+			if (val != null) {
+				Play(val.movie);
+			} else {
+				Play();
+			}
 		}
 	}
-	
+
+
 	public void Play() {
+		playing = true;
+	}
+	public void Play(int movie) {
+		this.movie = movie;
 		playing = true;
 	}
 }
---
+++ Assets/movieValue.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movieValue : MonoBehaviour {
+
+	public int movie = 0;
+}
